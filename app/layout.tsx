507dcc46d288fee2707778c9c877d4ba40ecadc8
alitkovsky <alitkovsky@me.com>
2024-01-05 17:49:09 +0100

    Added minor punktuation changes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/globals.css";
 
 export const metadata: Metadata = {
   title: 'Authentication',
@@ -11,5 +11,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-      {/* bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 */}
     </>
-  )
-}
+  );
+};
