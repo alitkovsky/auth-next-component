9f4d66a6cda60645612367ec3a0812c7132cad87
alitkovsky <alitkovsky@me.com>
2024-01-08 10:09:17 +0100

    Added schema for a register form.

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = zod.object({
       message: "Password is required",
    }),
 });
+
+export const RegisterSchema = zod.object({
+   name: zod.string().min(1, {
+      message: "Name is required",
+   }),
+   email: zod.string().email({
+      message: "Email is required",
+   }),
+   password: zod.string().min(8, {
+      message: "Minimum 8 characters required",
+   }),
+});
