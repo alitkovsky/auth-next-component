a489f430a785a2801dcd605c325b01111e40eedb
alitkovsky <alitkovsky@me.com>
2024-01-08 09:38:30 +0100

    Added server actions to throw error and success messages.

---
+++ actions/login.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import * as zod from "zod";
+import { LoginSchema } from "@/schemas";
+
+export const login = async (values: zod.infer<typeof LoginSchema>) => {
+   const validatedFields = LoginSchema.safeParse(values);
+
+   if(!validatedFields.success) {
+      return { error: "Invalid fields!" };
+   };
+
+   return { success: "Login complete!" };
+};
